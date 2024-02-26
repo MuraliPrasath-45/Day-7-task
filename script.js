99e9f85cd096f040443ebed88d128b4918860c39
Murali Prasath <70134163+MuraliPrasath-45@users.noreply.github.com>
2024-02-26 20:02:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var request=new XMLHttpRequest();
+1. a).  //var request=new XMLHttpRequest();
 request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
 request.send();
 request.onload=function(){
@@ -6,4 +6,28 @@ request.onload=function(){
     console.log(res);
     var result_asia=res.filter((ele)=>ele.region=="Asia");
     console.log(result_asia);
+}//
+
+
+b). //var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+request.send();
+request.onload=function(){
+    var res=JSON.parse(request.response);
+    console.log(res);
+    var result_pop=res.filter((ele)=>ele.population<200000);
+    console.log(result_pop);
 }
+    //
+
+
+d).  //var request=new XMLHttpRequest();
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json")
+request.send();
+request.onload=function(){
+    var res=JSON.parse(request.response);
+    console.log(res);
+    var total_pop=res.reduce((acc,cv)=>acc+cv.population,0)
+    console.log(total_pop);
+}
+    //
